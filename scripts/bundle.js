abdb320510a77f8d5787ff61ff01e2581a3b60fa
Pointy <pointyfar@gmail.com>
2020-02-23 23:10:31 +1000

    fix build scripts

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,9 +1,12 @@
 const fs = require('fs-extra');
-const zlib = require('zlib');
 const tar = require('tar');
+const pj = require('./../package.json');
+const version = pj.version;
+
 
 const indir = './dist/brixthis';
-const outdir = './dist/brixthis-bundle';
+const bundleDir = './dist/brixthis-bundle';
+const tarDest = `${indir}/brixthis-bundle.${version}.tgz`
 
 var assets = fs.readdirSync(indir).filter(fn => { return (!fn.endsWith('.js') && (!fn.endsWith('.html')))});
 var bx = fs.readdirSync(indir).filter(fn => { return fn.startsWith('brixthis')});
@@ -11,27 +14,43 @@ var bx = fs.readdirSync(indir).filter(fn => { return fn.startsWith('brixthis')})
 var files = assets.concat(bx);
 console.log(files)
 
-if (!fs.existsSync(outdir)){
-    fs.mkdirSync(outdir);
+if (!fs.existsSync(bundleDir)){
+    fs.mkdirSync(bundleDir);
 }
 
-files.forEach(file => {
-    fs.copy(`${indir}/${file}`, `${outdir}/${file}`, function (err) {
-        console.log('copying: ', file)
+
+function tarFiles(dirToCopy, outputFile) {
+
+return tar.c(
+      {
+        gzip: true,
+        file: outputFile
+      },
+      [dirToCopy]
+    ).then(() => {
+      console.log( 'Tarball has been created at', outputFile)
+      })
+}
+
+
+function copyFiles(){
+
+  let copied = 0;
+  for ( let i = 0; i < files.length; i++){
+    fs.copy(`${indir}/${files[i]}`, `${bundleDir}/${files[i]}`, function (err) {
+        console.log('copying: ', files[i])
         if (err) {
           console.error(err);
         } else {
-          console.log("copied:  ", file);
+          copied++;
+          console.log("copied:  ", files[i]);
+          if(copied == files.length ){
+            tarFiles(bundleDir, tarDest)
+          }
         }
       })
-})
+  }
+  
+}
 
-tar.c(
-    {
-      gzip: true,
-      file: `${indir}/brixthis-bundle.tgz`
-    },
-    [outdir]
-  ).then(() => {
-    console.log( 'Tarball has been created')
-    })
+copyFiles();
